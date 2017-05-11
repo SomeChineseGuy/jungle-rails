195ef0fa3f63b233b1d2b288778313050099c52f
vagrant <alvin.cl.ng@gmail.com>
2017-05-11 18:58:51 +0000

    Got the categories to work just gotta fix up the CSS to make the delete button with the correct categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,42 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+      @category = Category.new(categories_params)
+
+      if @category.save
+        redirect_to [:admin, :categories], notice: 'Categories created!'
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @category = Category.find params[:id]
+      @category.destroy
+      redirect_to [:admin, :categories], notice: 'Category deleted!'
+    end
+
+
+    private
+
+    def categories_params
+      params.require(:category).permit(
+        :name,
+        :description,
+        :category_id,
+        :quantity,
+        :image,
+        :price
+      )
+    end
+
+  end
+
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,9 @@
 class CategoriesController < ApplicationController
 
+
   def show
     @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+    @products = @category.categories.order(created_at: :desc)
   end
 
 end
