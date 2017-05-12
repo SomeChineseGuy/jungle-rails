29621157e71dc8969ea12b09a3d295da8c2fea30
vagrant <alvin.cl.ng@gmail.com>
2017-05-12 02:47:24 +0000

    Got the empty cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    UserMailer.user_email(order).deliver_now
   end
 
   def create
@@ -10,7 +11,6 @@ class OrdersController < ApplicationController
 
     if order.valid?
       empty_cart!
-      UserMailer.user_email(order).deliver_now
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, error: order.errors.full_messages.first
